36e5ddf49e63e00ff5e1bd959244603cde1cee20
vmarialuzm <vmarialuzm@gmail.com>
2025-03-15 12:24:40 -0500

    initialize my frontend project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold">hola mundo</h1>
+    </>
+  )
+}
+
+export default App
